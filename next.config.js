f85fe5ae89d8382076a2c6acf616b566e1e8856a
aevanvaghani <aevanvaghani5291@gmail.com>
2025-04-18 09:57:28 +0530

    Prepare project for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  basePath: '',
   images: {
     domains: ['localhost', 'aevan.infy.uk'],
     remotePatterns: [
@@ -11,10 +9,7 @@ const nextConfig = {
         hostname: '**',
       },
     ],
-    unoptimized: true,
   },
-  // Need this for GitHub pages AND custom domain
-  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
